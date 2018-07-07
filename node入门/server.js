bf43e0373cb26a0d174f4f21a6c5c97af368bb8c
Stephen <2655461060@qq.com>
2018-07-07 23:50:43 +1000

    2018.7.7 23:50 更新

--- node入门/server.js
+++ node入门/server.js
@@ -1,13 +1,19 @@
 const http = require('http')
 
-// http.Server 是一个类
-// http.createServer() 返回一个 http.Server 实例, 该实例有 server.listen() 方法, 用于开启服务器的监听
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-type': 'text/plain' })
-    res.write('Hello World')
-    res.end()
-})
+const start = () => {
+    // http.Server 是一个类
+    // http.createServer() 返回一个 http.Server 实例, 该实例有 server.listen() 方法, 用于开启服务器的监听
+    const server = http.createServer((req, res) => {
+        res.writeHead(200, { 'Content-type': 'text/plain' })
+        res.write('Hello World')
+        res.end()
+    })
 
-server.listen(8888, () => {
-    console.log('server listening at 8888');
-})
+    server.listen(8888, () => {
+        console.log('server listening at 8888');
+    })
+}
+
+module.exports = {
+    start: start
+}
