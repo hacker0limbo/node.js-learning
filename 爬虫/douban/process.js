3756a81d76eca5fc49cd65e6aa7a1a9680b2c948
Stephen <2655461060@qq.com>
2018-07-17 16:10:43 +1000

    2018.7.17 16:10 更新

--- 爬虫/douban/process.js
+++ 爬虫/douban/process.js
@@ -1,24 +1,29 @@
 const fs = require('fs')
 
-fs.readFile('./douban.json', (error, data) => {
-    if (error !== null) {
-        console.log('*** 读取文件失败');
-    } else {
-        // data 是一个 json 字符串
-        processFile(data)
-    }
-})
+
+const readFile = (path) => {
+    fs.readFile(path, (error, data) => {
+        if (error !== null) {
+            console.log('*** 读取文件失败');
+        } else {
+            // data 是一个 json 字符串
+            processFile(data)
+        }
+    })
+}
+
 
 const processFile = (data) => {
     const unIntactMovies = JSON.parse(data)
     console.log('数组长度为: ', unIntactMovies.length);
-    const intactMovies = []
+    let intactMovies = []
 
     for (let i = 0; i < unIntactMovies.length; i++) {
         const eachPage = unIntactMovies[i]
-        intactMovies.concat(eachPage)
+        intactMovies = intactMovies.concat(eachPage)
     }
     const newContent = JSON.stringify(intactMovies, null, 2)
+
     fs.writeFile('new-douban.json', newContent, (error) => {
         if (error !== null) {
             console.log('*** 写入新文件失败');
@@ -31,7 +36,7 @@ const processFile = (data) => {
 
 
 const __main = () => {
-
+    readFile('./douban.json')
 }
 
 __main()
