cdb0123eff97ed2894820d2b57328031f734c93b
Stephen <2655461060@qq.com>
2018-08-31 10:55:06 +1000

    update something

--- hello-world/index.js
+++ hello-world/index.js
@@ -6,26 +6,8 @@ const userRouter = require('./routes/about')
 app.use('/', indexRouter.router)
 app.use('/about', userRouter.router)
 
-// // 配置静态文件目录
-// app.use(express.static('public'))
-
-// // fs.readFile(路径, 回调函数(err, 文件内容))
-// const sendHtml = (path, response) => {
-//     var fs = require('fs')
-//     var options = {
-//         encoding: 'utf-8'
-//     }
-//     fs.readFile(path, options, (err, data) => {
-//         // console.log('数据类型为', typeof(data));
-//         response.send(data)
-//     })
-// }
-
-
-// app.get('/', function(request, response) {
-//     var path = 'index.html'
-//     sendHtml(path, response)
-// })
+// 配置静态文件目录
+app.use(express.static('public'))
 
 
 app.listen(3000, () => {
--- hello-world/routes/index.js
+++ hello-world/routes/index.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const router = express.Router()
 
+// fs.readFile(路径, 回调函数(err, 文件内容))
+const sentHtml = (path, res) => {
+    var fs = require('fs')
+    var options = {
+        encoding: 'utf-8'
+    }
+    fs.readFile(path, options, (err, data) => {
+        // console.log('数据类型为', typeof(data));
+        res.send(data)
+    })
+}
+
+
 router.get('/', (req, res) => {
-    res.send('home')
+    sentHtml('index.html', res)
 })
+
 
 module.exports = {
     router: router
