cf4ac90401a5403f6db3e044d3e79e3596e15e7b
Stephen <2655461060@qq.com>
2019-01-09 23:09:25 +1000

    update

--- blog/public/js/main.js
+++ blog/public/js/main.js
@@ -1,10 +1,10 @@
 const __main = () => {
     const api = new BlogApi()
     const blogTemplate = new BlogTemplate()
-    const c = new Controller(api, blogTemplate)
+    const controller = new Controller(api, blogTemplate)
     const addController = new AddController(api, blogTemplate)
     const textArea = new TextArea()
-    c.init()
+    controller.init()
 }
 
 __main()
