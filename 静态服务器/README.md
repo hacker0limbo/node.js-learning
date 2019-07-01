de4f458f9a0a4cb1a09f0b22a927f21c18b3ae31
Stephen <stephen.yin@outlook.com>
2019-07-01 21:44:27 +1000

    增加路由识别, 完成

--- 静态服务器/README.md
+++ 静态服务器/README.md
@@ -14,3 +14,13 @@ Resource interpreted as Stylesheet but transferred with MIME type text/plain: "h
 ```
 
 解决方法为可以写一个正则匹配后缀名, 根据不同的后缀名设置不同的 `content-type`
+
+## 路由匹配
+增加功能可以匹配路由, 基本思路为:
+
+- 解析 url, 得到 pathname 还有参数(查询字符串 query) /name?a=1&b=2
+- 根据 pathname 匹配路由函数, 如果存在
+  - 将请求时带有的参数(query)存入`req.query`里
+  - 监听请求那边到服务器的数据(比如 POST 方法), 存入`req.body`里
+  - 调用路由函数(`handleFn()`), 此时可以直接通过 req 这个对象获取刚刚传过来的数据了, 不管是 get 请求还是 post 请求
+- 静态服务器处理请求, 直接返回静态页面
--- 静态服务器/server.js
+++ 静态服务器/server.js
@@ -3,6 +3,36 @@ const path = require('path')
 const fs = require('fs')
 const url = require('url')
 
+const parseBody = (body) => {
+  // body 形式为 a=1&b=2
+  // 解析完毕为 {
+  //  a: 1,
+  //  b: 2,
+  // }
+  const o = {}
+  body.split('&').forEach(e => {
+    o[e.split('=')[0]] = e.split('=')[1]
+  })
+  return o
+}
+
+const routes = {
+  '/a': (req, res) => {
+    res.end(JSON.stringify(req.query))
+  },
+
+  '/a/c': (req, res) => {
+    res.end('路由为 /a/c')
+  },
+
+  '/search': (req, res) => {
+    for (const [k, v] of Object.entries(req.body)) {
+      res.write(`${k}=${v}, `)
+    }
+    res.end()
+  }
+}
+
 const staticRoot = (staticPath, req, res) => {
   // 请求可能为 localhost:3000/css/style.css?a=1, 那么 pathname 应该为 /css/style.css
   let reqPath = url.parse(req.url).pathname
@@ -28,8 +58,31 @@ const staticRoot = (staticPath, req, res) => {
   })
 }
 
+const routePath = (req, res) => {
+  const pathObj = url.parse(req.url)
+  const handler = routes[pathObj.pathname]
+  if (typeof handler === 'function') {
+    // 如果有对应的路由函数, 将 GET 请求里面附加的参数传递给 req.query
+    req.query = pathObj.query
+    let body = ''
+
+    // 接受 post 请求里面的数据
+    req.on('data', chunk => {
+      body += chunk
+    }).on('end', () => {
+      // 将 POST 请求里面发送到服务器里面的数据解析以后传递给 req.body
+      req.body = parseBody(body)
+      handler(req, res)
+    })
+  } else {
+    staticRoot(path.resolve(__dirname, 'static'), req, res)
+  }
+
+}
+
+
 const server = http.createServer((req, res) => {
-  staticRoot(path.join(__dirname, 'static'), req, res)
+  routePath(req, res)
 })
 
 server.listen(3000)
